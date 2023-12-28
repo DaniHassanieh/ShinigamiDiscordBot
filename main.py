89b7b1b1b8d8921f032e6151f9e8a43f36a79b28
DaniHassanieh <dioritoni@outlook.com>
2023-12-28 02:21:07 +0000

    We got it to work!! Time to figure it out later-

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-if __name__ == '__main__':
-  # run Shinigami
-  pass 
+from keep_alive import keep_alive
+import os
+import discord
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("I'm in")
+    print(client.user)
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        await message.channel.send(message.content[::-1])
+
+keep_alive()
+
+my_secret = os.environ['TOKEN']
+client.run(my_secret)
